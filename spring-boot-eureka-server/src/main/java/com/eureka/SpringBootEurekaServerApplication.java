15403ca40483ac78986354019229d94eaa678c4a
anderanjith@gmail.com <anderanjith@gmail.com>
2023-06-06 18:36:12 +0530

    space added

--- spring-boot-eureka-server/src/main/java/com/eureka/SpringBootEurekaServerApplication.java
+++ spring-boot-eureka-server/src/main/java/com/eureka/SpringBootEurekaServerApplication.java
@@ -8,6 +8,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 public class SpringBootEurekaServerApplication {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootEurekaServerApplication.class, args);
 	}
